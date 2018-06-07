b027fa5411d3b120595dc064edab2ba8d6ee44a3
taraskinnv <taraskin86@gmail.com>
2018-06-07 17:29:05 +0300

    final version
    final version

---
+++ WindowsForms_TreeView/WindowsForms_TreeView/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsForms_TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(330, 518);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.DoubleClick += new System.EventHandler(this.treeView1_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Rename";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(348, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(349, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 542);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsForms_TreeView/WindowsForms_TreeView/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FillDriveNodes();
+        }
+
+        private void FillDriveNodes()
+        {
+            treeView1.Nodes.Clear();
+            try
+            {
+                foreach (DriveInfo drive in DriveInfo.GetDrives())
+                {
+                    TreeNode driveNode = new TreeNode(drive.Name);
+                    FillTreeNode(driveNode, drive.Name);
+                    treeView1.Nodes.Add(driveNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void FillTreeNode(TreeNode driveNode, string path)
+        {
+            try
+            {
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    TreeNode dirNode = new TreeNode();
+                    dirNode.Text = dir.Remove(0, dir.LastIndexOf("\\") + 1);
+                    driveNode.Nodes.Add(dirNode);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            e.Node.Nodes.Clear();
+            try
+            {
+                if (Directory.Exists(e.Node.FullPath))
+                {
+                    var dirs = Directory.GetDirectories(e.Node.FullPath);
+                    var files = Directory.GetFiles(e.Node.FullPath);
+                    if (dirs.Length != 0)
+                    {
+                        foreach (var v in dirs)
+                        {
+                            TreeNode dirNode = new TreeNode(new DirectoryInfo(v).Name);
+                            FillTreeNode(dirNode, v);
+                            e.Node.Nodes.Add(dirNode);
+                        }
+                    }
+                    if (files.Length != 0)
+                    {
+                        foreach (var v in files)
+                        {
+                            TreeNode fileNode = new TreeNode(new DirectoryInfo(v).Name);
+                            FillTreeNode(fileNode, v);
+                            e.Node.Nodes.Add(fileNode);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private void treeView1_DoubleClick(object sender, EventArgs e)
+        {
+
+            Process.Start(treeView1.SelectedNode.FullPath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(treeView1.SelectedNode.FullPath);
+            if (file.Attributes == FileAttributes.Directory)
+            {
+                file.MoveTo(treeView1.SelectedNode.FullPath.Remove(treeView1.SelectedNode.FullPath.LastIndexOf("\\") + 1) + textBox1.Text);
+            }
+            else if (treeView1.SelectedNode.Text.IndexOf(file.Extension) == -1)
+            {
+                file.MoveTo(treeView1.SelectedNode.FullPath.Remove(treeView1.SelectedNode.FullPath.LastIndexOf("\\") + 1) + textBox1.Text + file.Extension);
+            }
+            else
+            {
+                file.MoveTo(treeView1.SelectedNode.FullPath.Remove(treeView1.SelectedNode.FullPath.LastIndexOf("\\") + 1) + textBox1.Text);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(treeView1.SelectedNode.FullPath);
+            if (dir.Attributes == FileAttributes.Directory)
+            {
+                Directory.Delete(treeView1.SelectedNode.FullPath, true);
+            }
+            else
+            {
+                FileInfo file = new FileInfo(treeView1.SelectedNode.FullPath);
+                file.Delete();
+            }
+        }
+
+
+
+    }
+}
+
+        
