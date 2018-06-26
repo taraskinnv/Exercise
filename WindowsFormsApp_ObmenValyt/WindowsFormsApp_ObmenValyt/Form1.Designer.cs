23372ed69fc4baa61fa3191c9998d3fe457cb995
taraskinnv <taraskin86@gmail.com>
2018-06-26 14:16:54 +0300

    ObmenValyt
    ObmenValyt
    parse with PB24

---
+++ WindowsFormsApp_ObmenValyt/WindowsFormsApp_ObmenValyt/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WindowsFormsApp_ObmenValyt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "UAH",
+            "USD",
+            "EUR",
+            "RUR"});
+            this.comboBox1.Location = new System.Drawing.Point(34, 42);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "UAH",
+            "USD",
+            "EUR",
+            "RUR"});
+            this.comboBox2.Location = new System.Drawing.Point(235, 42);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(235, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "На";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Отдадите";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Получите";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 319);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp_ObmenValyt/WindowsFormsApp_ObmenValyt/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_ObmenValyt
+{
+    public partial class Form1 : Form
+    {
+        string[] valyt;
+        public Form1()
+        {
+            InitializeComponent();
+            valyt = GetV();
+            //double[] znach = USD_UAH();
+            double[] znach = UAH("RUR");
+            //int d = RUR_UAH()[0];
+            //string[] wer = RUR_UAH();
+        }
+        
+
+        private string PB()     //получение строки с валютами
+        {
+            string str = "";
+            try
+            {
+                WebRequest request = WebRequest.Create("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11");
+
+                using (var response = request.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return str;
+        }
+
+        private string[] GetV()
+        {
+            string[] stringSeparators = new string[] { "}," };
+            return PB().Split(stringSeparators, StringSplitOptions.None);
+        }
+
+        private double[] USD_UAH()
+        {
+            double[] znach = new double[2];
+            znach[0] = ConvetrToDouble(valyt[0].Substring(valyt[0].IndexOf("\"buy\":\"") + "\"buy\":\"".Length, 8));
+            znach[1] = ConvetrToDouble(valyt[0].Substring(valyt[0].IndexOf("\"sale\":\"") + "\"sale\":\"".Length, 8));
+            return znach;
+        }
+
+        private double[] UAH(string Valut)
+        {
+            int i = -1;
+            switch (Valut)
+            {
+                case "USD":
+                    i = 0;
+                    break;
+                case "EUR":
+                    i = 1;
+                    break;
+                case "RUR":
+                    i = 2;
+                    break;
+            }
+            double[] znach = new double[2];
+            znach[0] = ConvetrToDouble(valyt[i].Substring(valyt[i].IndexOf("\"buy\":\"") + "\"buy\":\"".Length, valyt[i].IndexOf("\",\"sale") - (valyt[i].IndexOf("\"buy\":\"") + "\"buy\":\"".Length)));
+            znach[1] = ConvetrToDouble(valyt[i].Substring(valyt[i].IndexOf("\"sale\":\"") + "\"sale\":\"".Length, valyt[i].LastIndexOf("\"")-(valyt[i].IndexOf("\"sale\":\"") + "\"sale\":\"".Length)));
+            return znach;
+        }
+
+        private double[] EUR_UAH()
+        {
+            double[] znach = new double[2];
+            znach[0] = ConvetrToDouble(valyt[1].Substring(valyt[1].IndexOf("\"buy\":\"") + "\"buy\":\"".Length, 8));
+            znach[1] = ConvetrToDouble(valyt[1].Substring(valyt[1].IndexOf("\"sale\":\"") + "\"sale\":\"".Length, 8));
+            return znach;
+        }
+
+        private double[] RUR_UAH()
+        {
+            double[] znach = new double[2];
+            znach[0] = ConvetrToDouble(valyt[2].Substring(valyt[2].IndexOf("\"buy\":\"") + "\"buy\":\"".Length, 8));
+            znach[1] = ConvetrToDouble(valyt[2].Substring(valyt[2].IndexOf("\"sale\":\"") + "\"sale\":\"".Length, 8));
+            return znach;
+        }
+
+        //private double[] USD_UAH()
+        //{
+        //    double[] znach = new double[2];
+        //    znach[0] = ConvetrToDouble(valyt[0].Substring(valyt[0].IndexOf("\"buy\":\"") + "\"buy\":\"".Length, 8));
+        //    znach[1] = ConvetrToDouble(valyt[0].Substring(valyt[0].IndexOf("\"sale\":\"") + "\"sale\":\"".Length, 8));
+        //    return znach;
+        //}
+
+        private double ConvetrToDouble(string number)
+        {
+            string[] s = number.Split('.');
+            string sd = s[0] +','+ s[1];
+            return Double.Parse(sd);
+        }
+    }
+}
