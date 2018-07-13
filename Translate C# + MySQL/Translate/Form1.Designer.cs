8f9b56ea049866e8427e9e80e22afbc371d9dfb7
taraskinnv <taraskin86@gmail.com>
2018-07-13 12:12:54 +0300

    test
    test

---
+++ Translate C# + MySQL/Translate/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Translate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(687, 269);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(721, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(175, 264);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 429);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(687, 89);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(923, 13);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(138, 23);
+            this.Open.TabIndex = 4;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(920, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(920, 122);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 21);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(740, 412);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 89);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "save row";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(971, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(971, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "On";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(923, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(920, 233);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(138, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 317);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(687, 89);
+            this.textBox3.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Original text:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Your translation:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1073, 530);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.ComboBox comboBox2;
+        protected internal System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Translate C# + MySQL/Translate/Form1.cs
@@ -0,0 +1,397 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translate
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection context = new MySqlConnection("Database=bd_local_file; Datasource=localhost; User=root ;Password=ghbvf");
+
+        //DataClasses1DataContext context;
+        Ping pin = new Ping();
+        public Form1()
+        {
+            InitializeComponent();
+            context.Open();
+
+            //--------------------------------------------------------------------------
+            // Проверка наличия интернета
+            //IPStatus status = IPStatus.Unknown;
+            //PingReply pingReply = null;
+            //try
+            //{
+            //    pingReply = pin.Send("www.google.com");
+            //    textBox2.Text = pingReply.Address.ToString();
+            //    status = pingReply.Status;
+            //}
+            //catch (Exception) { }
+
+            //if (status != IPStatus.Success)
+            //{
+            //    MessageBox.Show("Please connect internet");
+            //    this.Close();
+            //}
+            //----------------------------------------------------------------------------
+            //наличие ID proc в базе
+            //context = new DataClasses1DataContext();
+
+            string str_proc = null;
+        //    var get_proc = context.Search_id_processor(GetID());
+        //    foreach (var i in get_proc)
+        //    {
+        //        str_proc = i.id_processor;
+        //    }
+
+        //    if (str_proc != GetID())
+        //    {
+        //        //оплата
+        //        MessageBox.Show("aaaaaaaaaaaaaaaaaaaaaa");
+        //        //this.Close();
+        //        context.Add_id_processor(GetID(), "qwerty");
+
+        //    }
+        //    //--------------------------------------------------------------------------------
+        //    //textBox2.Text = GetID();
+        //    if (Status() == 1)
+        //    {
+        //        textBox1.ReadOnly = true;
+        //        textBox3.ReadOnly = true;
+        //        label3.Text = label3.Text + " trial version!!!";
+        //        label4.Text = label4.Text + " trial version!!!";
+        //    }
+        }
+
+        int number = 0;
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //DataClasses1DataContext context = new DataClasses1DataContext();
+            //context.Add_id_processor(GetID(),"qwerty"); //для проверки своего ПК
+            dataGridView1.Columns.Add("word", "word");
+            dataGridView1.Columns.Add("translate", "translate");
+            dataGridView1.Columns[0].Width = 300;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.ReadOnly = true;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)            ////disable sort in DataGridView1
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            //var loc = context.Locate_language;
+            //foreach (var i in loc)
+            //{
+            //    comboBox1.Items.Add(i.languag);
+            //    comboBox2.Items.Add(i.languag);
+            //}
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 1;
+        }
+
+
+        List<string> list = new List<string>();
+        List<string> list2 = new List<string>();
+        List<string> list3 = new List<string>();
+        OpenFileDialog open;
+        string str_finish;
+        public string TranslateText(string input, string text, string output) //перевод google
+        {
+            string url = String.Format("http://www.google.com/translate_t?hl={0}&ie=UTF8&text={1}&langpair={2}", input, text, output);
+            WebClient webClient = new WebClient();
+            string result = webClient.DownloadString(url);
+            result = result.Substring(result.IndexOf("<span title=\"") + "<span title=\"".Length);
+            result = result.Substring(result.IndexOf(">") + 1);
+            result = result.Substring(0, result.IndexOf("</span>"));
+            return result.Trim();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Open file
+        {
+
+            list2.Clear();
+            open = new OpenFileDialog();
+            while (dataGridView1.RowCount != 1)     //очистка dataGridView1
+            {
+                dataGridView1.Rows.RemoveAt(0);
+            }
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader fs = new StreamReader(open.FileName);
+                StreamReader fs1 = new StreamReader(open.FileName);
+                str_finish = fs1.ReadToEnd();
+                string str;
+                do
+                {
+                    str = fs.ReadLine();
+                    if (str == null)
+                        break;
+                    if (str.IndexOf("msgid \"") != -1 && str.Length > 8)
+                    {
+                        str = str.Substring(str.IndexOf("msgid \"") + ("msgid \"").Length);
+                        str = str.Substring(0, str.Length - 1);
+                        if (str != null)
+                        {
+                            list2.Add(str);
+                        }
+                    }
+                    if (str.IndexOf("msgctxt \"") != -1 && str.Length > 10)
+                    {
+                        str = str.Substring(str.IndexOf("msgctxt \"") + "msgctxt \"".Length);
+                        str = str.Substring(0, str.Length - 1);
+                        if (str != null)
+                        {
+                            list2.Add(str);
+                        }
+                    }
+                } while (str != null);
+
+                IEnumerable<string> lll = list2.Distinct(); //убираю повторяющиеся слова
+                foreach (var i in lll)
+                {
+                    dataGridView1.Rows.Add(i, "");
+                }
+                list2.Clear();
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) // событие на изменение datagrid
+        {
+            if (e.ColumnIndex == 0)
+            {
+                //context = new DataClasses1DataContext();
+
+                //var id = context.Word_search_id(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString());
+                //int i1 = -1;
+                //foreach (var i in id)
+                //{
+                //    i1 = i.id;
+                //}
+                //if (i1 != -1)
+                //{
+                //    var mas = context.Word_tr(i1, comboBox1.Text);
+                //    foreach (var i in mas)
+                //    {
+                //        list.Add(i.word_slovo);
+                //    }
+                //}
+
+                list.Add(TranslateText(comboBox1.Text, dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString(), comboBox2.Text));
+
+
+                IEnumerable<string> list_b = list.Distinct();  //убираю повторяющиеся слова
+                foreach (var i in list_b)
+                {
+                    list3.Add(i);
+                }
+
+
+                listBox1.DataSource = null;
+                listBox1.DataSource = list3;
+                listBox1.DisplayMember = "list3";
+                list3.Clear();
+                list.Clear();
+                textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            }
+            else
+            {
+                list.Clear();
+                list3.Clear();
+                listBox1.DataSource = null;
+                listBox1.DataSource = list3;
+                listBox1.DisplayMember = "list3";
+            }
+        }
+
+        private void listBox1_Click(object sender, EventArgs e) //отобоажение для своего варианта
+        {
+            textBox1.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)    //добовление своего варианта  в dataGridView1
+        {
+            try
+            {
+                if (number<1 || Status() == 4)
+                {
+                    int rowindex = dataGridView1.CurrentCell.RowIndex;
+                    int columnindex = dataGridView1.CurrentCell.ColumnIndex;
+                    dataGridView1.Rows[rowindex].Cells[columnindex + 1].Value = textBox1.Text;
+                    number++;
+                }
+                else
+                {
+                    MessageBox.Show("Trial");
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)   //добовление c listBox1 в dataGridView1
+        {
+            try
+            {
+                if (number < 1 || Status() == 4)
+                {
+                    int rowindex = dataGridView1.CurrentCell.RowIndex;
+                    int columnindex = dataGridView1.CurrentCell.ColumnIndex;
+                    dataGridView1.Rows[rowindex].Cells[columnindex + 1].Value = listBox1.SelectedItem.ToString();
+                    number++;
+                }
+                else
+                {
+                    MessageBox.Show("Trial");
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)                  // Save file
+        {
+
+            if (Status() == 1)
+            {
+                MessageBox.Show("Trial");
+                return;
+            }
+
+            textBox2.Text = dataGridView1.RowCount.ToString();
+
+
+            //for (int i = 1; i < dataGridView1.RowCount; i++)        //проверка что все поля заполнены
+            //{
+            //    try
+            //    {
+            //        if (dataGridView1[1, i].Value.ToString() == "")
+            //        {
+            //            MessageBox.Show("Жопа");
+            //            return;
+            //        }
+            //    }
+            //    catch (Exception ex) { }
+            //}
+
+
+            for (int i = 0; i < 1/*dataGridView1.RowCount*/; i++)
+            {
+                str_finish = str_finish.Replace(dataGridView1[0, i].Value.ToString(), dataGridView1[1, i].Value.ToString());
+            }
+            SaveFileDialog save = new SaveFileDialog();
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    //-----------------------------------------------------------------------------------------------
+                    for (int i = 0; i < 1; i++) // проверка или добавление word в базу
+                    {
+                        Add_word_base(0, i, comboBox2.Text);
+                        Add_word_base(1, i, comboBox1.Text);
+                    }
+
+
+                    //// поиск Translate.id
+                    //int id_w1 = -1;
+                    //var id_word1 = context.Word_search_id(dataGridView1[0, 0].Value.ToString());
+                    //foreach (var i in id_word1)
+                    //{
+                    //    id_w1 = i.id;
+                    //}
+
+                    //int id_w2 = -1;
+                    //var id_translate1 = context.Word_search_id(dataGridView1[1, 0].Value.ToString());
+                    //foreach (var i in id_translate1)
+                    //{
+                    //    id_w2 = i.id;
+                    //}
+
+                    //var id_t = context.search_trans_id(comboBox1.Text, id_w1, id_w2);
+                    //int id_trans = -1;
+                    //foreach (var i in id_t)
+                    //{
+                    //    id_trans = i.id;
+                    //}
+
+                    //if (id_trans == -1)
+                    //{
+                    //    var loc1 = context.language_id(comboBox1.Text); //определение language_id
+                    //    int loc = -1;
+                    //    foreach (var l in loc1)
+                    //    {
+                    //        loc = l.id;
+                    //    }
+                    //    context.Add_transl(loc, id_w1, id_w2);    //добавление translate_word в базу
+                    //}
+                    //else
+                    //{
+                    //    context.Add_count_trans(id_trans);
+                    //}
+
+                    FileStream fs_write = new FileStream(save.FileName + ".pot", FileMode.Create);
+                    StreamWriter streamWriter = new StreamWriter(fs_write);
+                    streamWriter.Write(str_finish);
+                    streamWriter.Close();
+                
+
+            }
+        }
+
+        private void Add_word_base(int col, int row, string combobox)        //добавление слов в word
+        {
+            //int id = -1;
+            //var id_word = context.Word_search_id(dataGridView1[col, row].Value.ToString());
+            //foreach (var i in id_word)
+            //{
+            //    id = i.id;
+            //}
+
+            //if (id == -1)
+            //{
+            //    var loc1 = context.language_id(combobox); //определение language_id
+            //    int loc = -1;
+            //    foreach (var l in loc1)
+            //    {
+            //        loc = l.id;
+            //    }
+            //    context.Add_word(dataGridView1[col, row].Value.ToString(), loc);    //добавление слова в базу
+            //}
+        }
+
+        private int Status()
+        {
+            int status = 0;
+            //var get_proc = context.Search_status(GetID());
+            //foreach (var i in get_proc)
+            //{
+            //    status = i.status_id;
+            //}
+            return status;
+
+
+        }
+
+        private static string GetID()  //получение ID proc
+        {
+            string str = "";
+            ManagementClass mng = new ManagementClass("win32_processor");
+            ManagementObjectCollection collect = mng.GetInstances();
+            foreach (ManagementObject obj in collect)
+            {
+                str = obj.Properties["processorID"].Value.ToString();
+            }
+            return str;
+        }
+
+    }
+}
