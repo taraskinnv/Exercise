82a789a623c35f8d3b3c4bb3ca45fbd3c8267313
taraskinnv <taraskin86@gmail.com>
2018-10-18 18:32:24 +0300

    start zadanie
    start zadanie

---
+++ Zadanie_test/Zadanie_test/Document.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_test
+{
+    public class Document
+    {
+        string TMV { get; }  // товарно-материальные ценности
+        //string nomination { get; set; }
+        DateTimePicker Date_of_issue { get; set; }  // Дата выдачи
+        DateTimePicker Valid_until { get; set; }    // Действительна до .... (Дата)
+        public string Document_Number { get; set; }    // № документа
+        string Document_Date { get; set; }  // Дата документа
+        string issued_to { get; set; }  //Кому выдан
+        string Type_of_identity_Doc { get; set; }   // Тип документа удостоверяющего личность (паспорт)
+        string Doc_series { get; set; }     // Серия документа
+        string Doc_Number { get; set; }      //Номер документа
+        string Date_his_issue { get; set; } // Дата его выдачи
+        string Issued_by { get; set; }      // Кем выдан
+        string Organization { get; set; }   //Организация
+        string Contract { get; set; }   //Договор
+        int Amount { get; set; }    // Кол-во ТВЦ
+
+        public string Set_TMV(int number,string nomination, string unit, int amount)
+        {
+            return number + " " + nomination + " " + unit + " " + amount;
+        }
+
+        //public Document(string TMV, DateTimePicker Date_of_issue, DateTimePicker Valid_until, )
+        //{
+
+        //}
+
+        public Document()
+        {
+
+        }
+    }
+}
---
+++ Zadanie_test/Zadanie_test/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_test
+{
+    public partial class Form1 : Form
+    {
+        Form2 form2;
+        public Form1()
+        {
+            InitializeComponent();
+            //form2 = new Form2(this);
+            //form2.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("ТМЦ", "ТМЦ");
+            dataGridView1.Columns.Add("Кол-во", "Кол-во");
+            dataGridView1.Columns.Add("№ довер", "№ довер");
+            dataGridView1.Columns.Add("Дата выдачи", "Дата выдачи");
+            dataGridView1.Columns.Add("Действительна до", "Действительна до");
+            dataGridView1.Columns[0].Width = 300;
+            
+            dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.ReadOnly = true;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)            ////disable sort in DataGridView1
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            List<Document> doc = new List<Document>();
+            doc.Add(new Document());
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
