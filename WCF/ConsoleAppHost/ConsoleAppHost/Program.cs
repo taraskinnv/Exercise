a738557a8332a1055b324f95531985c05e411f6e
taraskinnv <taraskin86@gmail.com>
2018-10-27 12:18:02 +0300

    ver.1
    ver.1

---
+++ WCF/ConsoleAppHost/ConsoleAppHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Хост";
+            Uri address = new Uri("http://localhost:8733/ConsoleAppHost/IContract");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            Type contract = typeof(IContract);
+            ServiceHost host = new ServiceHost(typeof(Service));
+            host.AddServiceEndpoint(contract, binding, address);
+            host.Open();
+            Console.WriteLine("Хост готов к приему");
+            Console.ReadLine();
+            host.Close();
+            Console.WriteLine("Хост завершил работу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WCF/ConsoleAppHost/WinFormsAppClient/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsAppClient
+{
+
+    public partial class Form1 : Form
+    {
+        IContract chanel;
+        public Form1()
+        {
+            InitializeComponent();
+            Uri address =
+               new Uri("http://localhost:8733/ConsoleAppHost/IContract");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            EndpointAddress endpoint = new EndpointAddress(address);
+            ChannelFactory<IContract> factory =
+                new ChannelFactory<IContract>(binding, endpoint);
+            chanel = factory.CreateChannel();
+        }
+
+        private void CalcString()
+        {
+            int num = chanel.NumberLines(textBox1.Text);
+            MessageBox.Show(string.Format("В предложении:\"{0}\" количество слов = {1}", textBox1.Text, num), "Вычисление количества слов");
+        }
+        private void CalcGlas()
+        {
+            int num = chanel.NumberGlas(textBox1.Text);
+            MessageBox.Show(string.Format("В предложении:\"{0}\" гласных букв = {1}", textBox1.Text, num), "Вычисление гласных букв");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CalcGlas();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Клиент";
+        }
+    }
+}
